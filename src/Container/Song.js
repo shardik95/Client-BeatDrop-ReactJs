0c4cd7d9c29269be489d3a057c117173f46d6ec4
shardik95 <shardik95@ccs.neu.edu>
2018-06-25 23:37:16 -0400

    bug fixes

--- src/Container/Song.js
+++ src/Container/Song.js
@@ -75,21 +75,31 @@ class Song extends React.Component{
         let trackId=newProps.match.params.trackId;
         this.setState({trackId:trackId})
 
-        this.spotifyService.getAccessToken()
-            .then(response=> (this.setState({accessToken: response.access_token})))
-            .then(()=>this.spotifyService.getTrackById(this.state.trackId,this.state.access_token))
+        fetch("https://beatdrop.herokuapp.com/api/accessToken")
+            .then(response=>(
+                response.json()
+            )).then(response=> {
+            return this.setState({accessToken: response.access_token})
+        }).then(()=>fetch('https://api.spotify.com/v1/tracks/'+this.state.trackId,{
+            headers:{
+                'Authorization':'Bearer '+this.state.accessToken
+            }
+        }))
+            .then((response)=>response.json())
             .then((song)=>this.setState({song:song}))
 
-
-        this.userService.getSession()
+        fetch("https://beatdrop.herokuapp.com/api/profile",{
+            credentials: 'include',
+        }).then((response)=>response.json())
             .then((json)=>(this.setState({user:json})))
             .then(()=>this.state.user.userName!=='CANNOT FIND'&&this.state.user.likes.map(like=>{
-                if(like.trackId===this.state.trackId)
-                    this.setState({setLike:true,like:like})
+                if(like.typeId===this.state.trackId) {
+                    this.setState({setLike: true,like:like})
+                }
             }))
             .then(()=>this.state.user.userName!=='CANNOT FIND'&&this.state.user.reviews.map(review=>{
                 if(review.typeId===this.state.trackId) {
-                    this.setState({rating:review.stars})
+                    this.setState({rating:review.stars,reviewId:review.id})
                 }
             }))
 
